f808a4c1cd62fabe3cc8db97b44250deca328c94
skykhs3 <skykhs3@naver.com>
2020-03-12 18:34:58 +0900

    firebase-tutorial

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAz645k59ZB9J27iE7vvYZss4j3s1l39XI",
+    authDomain: "fir-test-6e6c7.firebaseapp.com",
+    databaseURL: "https://fir-test-6e6c7.firebaseio.com",
+    projectId: "fir-test-6e6c7",
+    storageBucket: "fir-test-6e6c7.appspot.com",
+    messagingSenderId: "333804642680",
+    appId: "1:333804642680:web:eaac9e415eb0d45e63a88d",
+    measurementId: "G-K9RQMJYS38"
+  };
+  export default firebaseConfig;
